7581fe1db21ea76123f0e07032390cdd4eff0b88
Yaroslav Sychev <fsych1@icloud.com>
2024-07-20 13:03:15 +0700

    Time: 75 ms (16.59%), Space: 49.4 MB (58.13%) - LeetHub

---
+++ 2862-interval-cancellation/2862-interval-cancellation.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+ 
+var cancellable = function(fn, args, t) {
+    
+    fn(...args);
+    
+    let id = setInterval(() => {
+        fn(...args);
+    },t);
+
+    let cancelFn = () => clearInterval(id);
+
+    return cancelFn;
+};
