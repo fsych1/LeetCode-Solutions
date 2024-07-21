11246fd57a3d346d10f47a10309b17be61357029
Yaroslav Sychev <fsych1@icloud.com>
2024-07-22 01:11:11 +0700

    Time: 68 ms (9.76%), Space: 52.3 MB (10.76%) - LeetHub

---
+++ 2798-chunk-array/2798-chunk-array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array[]}
+ */
+function chunk(inputArray, chunkSize) {
+  const chunks = [];
+
+  for (let i = 0; i < inputArray.length; i += chunkSize) {
+    const chunk = [];
+
+    for (let j = i; j < Math.min(i + chunkSize, inputArray.length); j++) {
+      chunk.push(inputArray[j]);
+    }
+
+    chunks.push(chunk);
+  }
+  
+  return chunks;
+}
