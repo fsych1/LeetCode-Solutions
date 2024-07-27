cc8305608f0548cb5fb854449e07b56153a2e7bb
Yaroslav Sychev <fsych1@icloud.com>
2024-07-28 00:29:13 +0700

    Time: 169 ms (27.57%), Space: 78.6 MB (40.51%) - LeetHub

---
+++ 2759-flatten-deeply-nested-array/2759-flatten-deeply-nested-array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+function flat(arr, n) {
+  const result = []; 
+
+  for (let item of arr) {
+    if (Array.isArray(item) && n > 0) {
+      result.push(...flat(item, n - 1)); 
+    } else {
+      result.push(item); 
+    }
+  }
+
+  return result;
+}
